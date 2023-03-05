dad70f9a6ba05a9ce03ac131e0d075a0c7ecff02
Naldo <naldorck@gmail.com>
2023-03-05 18:39:07 -0500

    mount context provider

--- src/context/contextApi/actionsTypes/AlegraActionTypes.ts
+++ src/context/contextApi/actionsTypes/AlegraActionTypes.ts
@@ -5,6 +5,20 @@
  * @author Naldo Duran <naldorck@gmail.com> *
  * @returns {React.Context<ISeller[]>}
  */
+import { Dispatch } from "react"
+import { ISeller } from "@ctypes/alegra.td"
+
+type ContextPropType<State, DispactActions> = IContextProps<State, DispactActions>
+
+interface IContextProps<State, DispactActions> {
+  state: State
+  dispatch: Dispatch<DispactActions>
+}
+
+export type GlobalStateType = {
+  sellers: ContextPropType<Array<ISeller>, any>
+}
+
 export const AlegraActionTypes = {
   seller_add_all: "[alegra] add all sellers",
   seller_add_vote: "[alegra] add 1 vote",
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { DashboardPage } from "@pages"
+import AlegraProvider from "@contextApi/providers/AlegraContext"
 import "@/App.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <DashboardPage />
+    <AlegraProvider>
+      <DashboardPage />
+    </AlegraProvider>
   </React.StrictMode>,
 )
