1e7be3bc7768f81537e0798d972f5e44d5a5c12f
Naldo <naldorck@gmail.com>
2023-03-07 17:38:50 -0500

    add bill page

---
+++ src/pages/Bill.tsx
@@ -0,0 +1,22 @@
+/**
+ * Page: Dashboard
+ *
+ * @version 1.0.1
+ * @author Naldo Duran <naldorck@gmail.com> *
+ * @returns {React.FC}
+ */
+
+const Bill: React.FC = () => {
+  const template = (
+    <section className="container">
+      <article>
+        <h1 className="text-title text-center">Factura de venta</h1>
+      </article>
+      {/* <!-- /> */}
+    </section>
+  )
+
+  return template
+}
+
+export default Bill
