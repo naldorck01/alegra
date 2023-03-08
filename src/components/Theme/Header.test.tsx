abd1615b3dfd88257908fa6015fe33767b9182f3
naldorck01 <naldorck@gmail.com>
2023-03-07 22:46:23 -0800

    test: incomplete test

--- src/components/Theme/Header.test.tsx
+++ src/components/Theme/Header.test.tsx
@@ -6,17 +6,17 @@
  * @returns {void}
  */
 import { describe } from "vitest"
-import { render, screen } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import Header from "./Header"
 
 describe("Header", () => {
-
-  beforeEach(() => {
-
-  })
-
   test("should render the component", () => {
     const { container } = render(<Header />)
-    expect(container.querySelector("headers")).toBeDefined()
+    expect(container.querySelector("header")).not.toBeNull()
+  })
+
+  test("should contains the Alegra logo", () => {
+    const { container } = render(<Header />)
+    expect(container.querySelector("img[alt='Logo Alegra']")).not.toBeNull()
   })
 })
